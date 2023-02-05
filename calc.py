798fefb83a5ccf19e8bb51b5bfc0decc2ec30145
hannushchenko <hannushchenko@gmail.com>
2023-02-05 15:54:12 +0200

    added hex(16>10 calc

--- calc.py
+++ calc.py
@@ -1,22 +1,47 @@
-def num2calc():
-    inputnum = input("enter 2 num: ")
-    list2calc = list(inputnum)
-    len2calc = len(list2calc)
+# def num2calc():
+#     inputNum = input("enter 2 num: ")
+#     list2calc = list(inputNum)
+#     len2calc = len(list2calc)
+#     res = 0
+#     work = True
+#
+#     for n in list2calc:
+#         num = int(n)
+#         if num == 1 or num == 0:
+#             res = res + num * pow(2, len2calc-1)
+#             len2calc = len2calc - 1
+#         else:
+#             print("error")
+#             work = False
+#             break
+#
+#     if work:
+#         print(res)
+#
+# num2calc()
+
+def num16calc():
+    checkList16num = ['0','1','2','3',
+                      '4','5','6','7',
+                      '8','9','a','b',
+                      'c','d','e','f']
+
+    inputNum = input("enter num16: ")
     res = 0
     work = True
+    inputNumLen = len(inputNum)
 
-    for n in list2calc:
-        num = int(n)
-        if num == 1 or num == 0:
-            res = res + num * pow(2, len2calc-1)
-            len2calc = len2calc - 1
+    for symb in inputNum:
+        if str(symb) in checkList16num:
+            intSymb = checkList16num.index(symb)
+            res = res + intSymb * pow(16, inputNumLen-1)
+            inputNumLen = inputNumLen - 1
         else:
             print("error")
             work = False
             break
-
     if work:
         print(res)
 
-num2calc()
+num16calc()
 
