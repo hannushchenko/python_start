2ea37968734ff019c6f76a48d09edce9a5a4e14e
hannushchenko <hannushchenko@gmail.com>
2023-02-16 02:57:47 +0200

    championship is ready

--- football.py
+++ football.py
@@ -1,17 +1,37 @@
 import random
 
 teams = ['arsenal', 'chelsea', 'manchester city', 'liverpool', 'everton', 'manchester united', 'tottenham']
+
+
 pairs = []
-for home in teams:
-    for guest in teams:
-        if home != guest:
+for home_name in teams:
+    for guest_name in teams:
+        winner = None
+        if home_name != guest_name:
             home_score = random.randint(0, 4)
             guest_score = random.randint(0, 4)
-            pairs.append({home: home_score, guest: guest_score })
+            if home_score > guest_score:
+                winner = home_name
+            elif home_score < guest_score:
+                winner = guest_name
+            else:
+                pass
+            new_pair = {'home_name': home_name,
+                          'home_score': home_score,
+                          'guest_name': guest_name,
+                          'guest_score': guest_score,
+                           'winner': winner}
+            pairs.append(new_pair)
+            print(*new_pair.values(), sep=" - ")
 
 
-
-for index, pair in enumerate(pairs):
-    print(index)
-    print(pair.items())
-
+teams_res = {}
+for i in teams:
+    teams_res[i] = 0
+    for pair in pairs:
+        if pair['winner'] == i:
+            teams_res[i]+=3
+        elif pair['winner'] == None and (pair['home_name'] == i or pair['guest_name'] == i):
+            teams_res[i]+=1
+print("=================================================")
+print(*sorted(teams_res.items(), key = lambda x: x[1], reverse=True))
